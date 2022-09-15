d560de181f0de84262d749608ed130f403bd7845
carmen <58992828+cgbl-90@users.noreply.github.com>
2022-09-15 17:51:17 +0200

    Added comment
    Use filter + map for forecast logic

--- src/ForecastData.js
+++ src/ForecastData.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function printForecast(forecastData) {
-  /* I am unable to get the forecast */
+  /* 
+    I am unable to get the forecast data
+
+    Use methods filter and map together to avoid printing when the forecast is empty
+
+    forecast.filter (day => day.id !== 0). map (...)
+
+  */
 
   return " ";
 }
